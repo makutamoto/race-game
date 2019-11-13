24b37bf690872dd23a3b2d7206d84a39494d93d2
Makutamoto <34860673+makutamoto@users.noreply.github.com>
2019-11-14 00:41:31 +0900

    Added a mini map and improved camera control.

--- main.c
+++ main.c
@@ -33,23 +33,26 @@ static FontSJIS shnm16b;
 static Controller keyboard;
 static ControllerEvent wasd[4], action, restart, collision, quit, arrow[4], backCamera;
 
-static Image hero;
+static Image hero, opponentImage;
 static Image course;
 static Image stageImage;
 
 static Scene scene;
 static Node speedNode, lapNode, rankNode, centerNode, timeNode;
+static Node mapNode;
 static Node lapJudgmentNodes[3];
 static Node heroNode, heroRayNode;
 static Node opponentNode, opponentRayNode;
-static Node courseNode, courseDirtNode;
+static Node heroMarkerNode, opponentMarkerNode;
+static Node courseNode, courseMapNode, courseDirtNode;
 static Node stageNode;
 
 static Scene resultScene;
 static Node resultNode;
 
-static float nextCameraPosition[3] = { 0.0F, 25.0F, -50.0F };
-static float currentCameraPosition[3] = { 0.0F, 25.0F, -50.0F };
+static Scene mapScene;
+
+static float defaultCameraPosition[3] = { 0.0F, 25.0F, -50.0F };
 static float cameraAngle;
 static int heroLapScore, opponentLapScore;
 static int heroPreviousLap = -1, opponentPreviousLap = -1;
@@ -58,16 +61,54 @@ static int rank;
 static int isfinished;
 static float transition;
 static LARGE_INTEGER startTime;
+static float handle;
 
-static int autoDrive(Node *node) {
-	float tempVec3[2][3];
+static float controlCar(Node *node, float accel, float handle) {
+	float tempVec3[3][3];
+	float tempMat3[1][3][3];
+	float tempMat4[1][4][4];
+	float velocityLengthH;
+	convMat4toMat3(genRotationMat4(node->angle[0], node->angle[1], node->angle[2], tempMat4[0]), tempMat3[0]);
+	initVec3(tempVec3[0], Z_MASK);
+	mulMat3Vec3(tempMat3[0], tempVec3[0], tempVec3[1]);
+	normalize3(extractComponents3(tempVec3[1], XZ_MASK, tempVec3[0]), tempVec3[1]);
+	extractComponents3(node->velocity, XZ_MASK, tempVec3[0]);
+	velocityLengthH = length3(tempVec3[0]);
+	mulVec3ByScalar(tempVec3[1], velocityLengthH * node->shape.mass + accel * 150000.0F, tempVec3[0]);
+	subVec3(tempVec3[0], mulVec3ByScalar(node->velocity, node->shape.mass, tempVec3[2]), tempVec3[1]);
+	applyForce(node, tempVec3[1], XZ_MASK, FALSE);
+	if(velocityLengthH > 1.0F) node->torque[1] += handle * max(velocityLengthH * 5000.0F, 2500000.0F);
+	initVec3(tempVec3[0], Y_MASK);
+	mulMat3Vec3(tempMat3[0], tempVec3[0], tempVec3[1]);
+	initVec3(tempVec3[0], Y_MASK);
+	return cosVec3(tempVec3[0], tempVec3[1]);
+}
+
+static int autoDrive(Node *node, float normal[3]) {
+	float tempVec3[3][3];
+	float tempMat3[1][3][3];
+	float tempMat4[1][4][4];
+	float front[3];
 	Node *ray = node->children.firstItem->data;
 	if(ray->collisionTargets.length != 0) {
 		CollisionInfo *rayInfo = ray->collisionTargets.firstItem->data;
 		CollisionInfoNode2Node *info = rayInfo->info.firstItem->data;
-		subVec3(mulVec3ByScalar(info->normal, -10000.0F, tempVec3[0]), mulVec3ByScalar(node->velocity, node->shape.mass, tempVec3[1]), tempVec3[0]);
-		applyForce(node, tempVec3[0], XZ_MASK, FALSE);
+		memcpy_s(normal, SIZE_VEC3, info->normal, SIZE_VEC3);
 	}
+	convMat4toMat3(genRotationMat4(node->angle[0], node->angle[1], node->angle[2], tempMat4[0]), tempMat3[0]);
+	initVec3(tempVec3[0], Z_MASK);
+	mulMat3Vec3(tempMat3[0], tempVec3[0], front);
+	tempVec3[1][0] = front[0];
+	tempVec3[1][1] = front[2];
+	tempVec3[1][2] = 0.0F;
+	clearVec3(tempVec3[0]);
+	tempVec3[0][0] = normal[0];
+	tempVec3[0][1] = normal[2];
+	cross(tempVec3[0], tempVec3[1], tempVec3[2]);
+	// node->torque[1] += 300000.0F *  * (1.0F - cosVec3(normal, tempVec3[1])) / 2.0F;
+	// subVec3(mulVec3ByScalar(front, 30000.0F, tempVec3[0]), mulVec3ByScalar(node->velocity, node->collisionShape.mass, tempVec3[1]), tempVec3[0]);
+	// applyForce(node, tempVec3[0], XZ_MASK, FALSE);
+	controlCar(node, (node->collisionFlags & COURSE_COLLISIONMASK) ? -0.5F : 1.0F, -sign(tempVec3[2][2]) / 2.0F);
 	return TRUE;
 }
 
@@ -98,26 +139,20 @@ static void updateLapScore(unsigned int flags, int *previousLap, int *lapScore) 
 	}
 }
 
+static float force[3];
 static int heroBehaviour(Node *node) {
-	float tempVec3[3][3];
-	float tempMat3[1][3][3];
-	float tempMat4[1][4][4];
-	CollisionInfo *targetInfo;
 	if(node->collisionFlags & COURSE_COLLISIONMASK) {
 		if(isfinished) {
-			autoDrive(node);
+			autoDrive(node, force);
 		} else {
-			float velocityLengthH;
-			convMat4toMat3(genRotationMat4(node->angle[0], node->angle[1], node->angle[2], tempMat4[0]), tempMat3[0]);
-			initVec3(tempVec3[0], Z_MASK);
-			mulMat3Vec3(tempMat3[0], tempVec3[0], tempVec3[1]);
-			normalize3(extractComponents3(tempVec3[1], XZ_MASK, tempVec3[0]), tempVec3[1]);
-			extractComponents3(node->velocity, XZ_MASK, tempVec3[0]);
-			velocityLengthH = length3(tempVec3[0]);
-			mulVec3ByScalar(tempVec3[1], velocityLengthH * node->shape.mass + controller.move[1] * 30000.0F, tempVec3[0]);
-			subVec3(tempVec3[0], mulVec3ByScalar(node->velocity, node->shape.mass, tempVec3[2]), tempVec3[1]);
-			applyForce(node, tempVec3[1], XZ_MASK, FALSE);
-			if(velocityLengthH > 1.0F) node->torque[1] += controller.move[0] * max(velocityLengthH * 1000.0F, 500000.0F);
+			if(controlCar(node, controller.move[1], handle) < 0.0F) {
+				node->angle[0] = 0.0F;
+				node->angle[2] = 0.0F;
+				node->angVelocity[0] = 0.0F;
+				node->angVelocity[2] = 0.0F;
+				node->angMomentum[0] = 0.0F;
+				node->angMomentum[2] = 0.0F;
+			}
 		}
 	}
 	if(node->collisionFlags & DIRT_COLLISIONMASK) {
@@ -126,30 +161,29 @@ static int heroBehaviour(Node *node) {
 		node->collisionShape.dynamicFriction = 0.1F;
 	}
 	updateLapScore(node->collisionFlags, &heroPreviousLap, &heroLapScore);
-	iterf(&node->collisionTargets, &targetInfo) {
-		if(targetInfo->target->collisionMaskActive & LAP_COLLISIONMASK) {
-			CollisionInfoNode2Node *info = targetInfo->info.firstItem->data;
-			float upper[3] = { 0.0F, 25.0F, 0.0F };
-			mulVec3ByScalar(info->normal, 50.0F, nextCameraPosition);
-			addVec3(nextCameraPosition, upper, nextCameraPosition);
-		}
-	}
+	memcpy_s(heroMarkerNode.position, SIZE_VEC3, node->position, SIZE_VEC3);
+	memcpy_s(heroMarkerNode.angle, SIZE_VEC3, node->angle, SIZE_VEC3);
 	return TRUE;
 }
 
+static float onormal[3];
 static int opponentBehaviour(Node *node) {
 	updateLapScore(node->collisionFlags, &opponentPreviousLap, &opponentLapScore);
-	autoDrive(node);
+	autoDrive(node, onormal);
+	memcpy_s(opponentMarkerNode.position, SIZE_VEC3, node->position, SIZE_VEC3);
+	memcpy_s(opponentMarkerNode.angle, SIZE_VEC3, node->angle, SIZE_VEC3);
 	return TRUE;
 }
 
 static int timeBehaviour(Node *node) {
-	char buffer[14];
-	float elapsed = elapsedTime(startTime);
-	int minutes = elapsed / 60;
-	float seconds = elapsed - 60.0F * minutes;
-	sprintf(buffer, "TIME %d`%06.3f", minutes, seconds);
-	drawTextSJIS(node->texture, shnm12, 0, 0, buffer);
+	if(!isfinished) {
+		char buffer[14];
+		float elapsed = elapsedTime(startTime);
+		int minutes = elapsed / 60;
+		float seconds = elapsed - 60.0F * minutes;
+		sprintf(buffer, "TIME %d`%06.3f", minutes, seconds);
+		drawTextSJIS(node->texture, shnm12, 0, 0, buffer);
+	}
 	return TRUE;
 }
 
@@ -161,15 +195,16 @@ static int speedBehaviour(Node *node) {
 }
 
 static int lapBehaviour(Node *node) {
-	char buffer[8];
-	int lap = heroLapScore / 45 + 1;
-	if(lap > 3) {
-		lap = 3;
-		isfinished = TRUE;
-		transition += min(transition * transition + 0.01F, 1.0F);
+	if(!isfinished) {
+		char buffer[8];
+		int lap = heroLapScore / 45 + 1;
+		if(lap > 3) {
+			lap = 3;
+			isfinished = TRUE;
+		}
+		sprintf(buffer, "LAP %d/3", lap);
+		drawTextSJIS(node->texture, shnm12, 0, 0, buffer);
 	}
-	sprintf(buffer, "LAP %d/3", lap);
-	drawTextSJIS(node->texture, shnm12, 0, 0, buffer);
 	return TRUE;
 }
 
@@ -186,6 +221,8 @@ static int centerBehaviour(Node *node) {
 	if(isfinished) {
 		drawTextSJIS(node->texture, shnm16b, 0, 0, "FINISH!");
 		node->isVisible = TRUE;
+	} else {
+		node->isVisible = FALSE;
 	}
 	return TRUE;
 }
@@ -198,13 +235,19 @@ static int resultBehaviour(Node *node) {
 static Node initCarRayNode(const char id[]) {
 	Node carRayNode;
 	carRayNode = initNode(id, NO_IMAGE);
-	carRayNode.shape = initShapePlane(30.0F, 150.0F, BLACK, 1.0F);
+	carRayNode.shape = initShapePlane(75.0F, 150.0F, BLACK, 1.0F);
 	carRayNode.position[2] = 75.0F;
 	carRayNode.collisionShape = carRayNode.shape;
-	carRayNode.collisionMaskPassive = LAPA_COLLISIONMASK | LAPB_COLLISIONMASK | LAPC_COLLISIONMASK;
+	carRayNode.collisionMaskPassive = LAP_COLLISIONMASK | COURSE_COLLISIONMASK;
 	carRayNode.isThrough = TRUE;
 	carRayNode.isVisible = FALSE;
 	return carRayNode;
+}
+
+static int mapBehaviour(Node *node) {
+	node->texture = drawScene(&mapScene);
+	node->texture.transparent = BLACK;
+	return TRUE;
 }
 
 static void initialize(void) {
@@ -228,11 +271,10 @@ static void initialize(void) {
 	pushUntilNull(&keyboard.events, &action, &restart, &quit, &collision, &backCamera, NULL);
 
 	hero = loadBitmap("assets/subaru.bmp", NULL_COLOR);
+	opponentImage = loadBitmap("assets/subaru2p.bmp", NULL_COLOR);
 	course = loadBitmap("assets/courseMk2.bmp", NULL_COLOR);
 	scene = initScene();
-	scene.camera = initCamera(0.0F, 50.0F, -50.0F, 1.0F);
 	scene.camera.parent = &heroNode;
-	scene.camera.isRotationDisabled = TRUE;
 	scene.camera.positionMask[1] = TRUE;
 	scene.background = BLUE;
 
@@ -259,26 +301,26 @@ static void initialize(void) {
 	setVec3(stageNode.scale, 4.0F, XYZ_MASK);
 
 	heroNode = initNode("Hero", hero);
-	initShapeFromObj(&heroNode.shape, "./assets/subaru.obj", 100.0F);
-	initShapeFromObj(&heroNode.collisionShape, "./assets/subaruCollision.obj", 100.0F);
+	initShapeFromObj(&heroNode.shape, "./assets/subaru.obj", 1.0F);
+	initShapeFromObj(&heroNode.collisionShape, "./assets/subaruCollision.obj", 500.0F);
 	heroNode.isPhysicsEnabled = TRUE;
 	setVec3(heroNode.scale, 4.0F, XYZ_MASK);
 	heroNode.collisionMaskActive = CAR_COLLISIONMASK;
 	heroNode.collisionMaskPassive = CAR_COLLISIONMASK | COURSE_COLLISIONMASK | LAP_COLLISIONMASK | DIRT_COLLISIONMASK;
 	heroNode.behaviour = heroBehaviour;
 	heroRayNode = initCarRayNode("heroRay");
-	push(&heroNode.children, &heroRayNode);
+	addNodeChild(&heroNode, &heroRayNode);
 
-	opponentNode = initNode("opponent", hero);
-	initShapeFromObj(&opponentNode.shape, "./assets/subaru.obj", 100.0F);
-	initShapeFromObj(&opponentNode.collisionShape, "./assets/subaruCollision.obj", 100.0F);
+	opponentNode = initNode("opponent", opponentImage);
+	initShapeFromObj(&opponentNode.shape, "./assets/subaru.obj", 1.0F);
+	initShapeFromObj(&opponentNode.collisionShape, "./assets/subaruCollision.obj", 500.0F);
 	opponentNode.isPhysicsEnabled = TRUE;
 	setVec3(opponentNode.scale, 4.0F, XYZ_MASK);
 	opponentNode.collisionMaskActive = CAR_COLLISIONMASK;
 	opponentNode.collisionMaskPassive = CAR_COLLISIONMASK | COURSE_COLLISIONMASK | LAPA_COLLISIONMASK | LAPB_COLLISIONMASK | LAPC_COLLISIONMASK;
 	opponentNode.behaviour = opponentBehaviour;
 	opponentRayNode = initCarRayNode("opponentRay");
-	push(&opponentNode.children, &opponentRayNode);
+	addNodeChild(&opponentNode, &opponentRayNode);
 
 	for(i = 0;i < 3;i++) {
 		lapJudgmentNodes[i] = initNode(lapNames[i], NO_IMAGE);
@@ -297,25 +339,56 @@ static void initialize(void) {
 	centerNode = initNodeText("center", 32.0F, 56.0F, 56, 16, centerBehaviour);
 	centerNode.isVisible = FALSE;
 
+	mapNode = initNodeText("map", 0.0F, 24.0F, 128.0F, 128.0F, mapBehaviour);
+	divVec3ByScalar(mapNode.scale, 4.0F, mapNode.scale);
+
 	resultScene = initScene();
 	resultNode = initNodeText("result", 0.0F, 0.0F, 84, 36, resultBehaviour);
 	push(&resultScene.nodes, &resultNode);
+
+	mapScene = initScene();
+	mapScene.camera = initCamera(1.0F, 1000.0F, 0.0F, 1.0F);
+	mapScene.camera.farLimit = 1000.0F;
+
+	courseMapNode = courseNode;
+	courseMapNode.texture = initImage(1, 1, WHITE, NULL_COLOR);
+	push(&mapScene.nodes, &courseMapNode);
+
+	heroMarkerNode = initNode("heroMarker", NO_IMAGE);
+	heroMarkerNode.shape = initShapeBox(250.0F, 250.0F, 250.0F, DARK_BLUE, 1.0F);
+	push(&mapScene.nodes, &heroMarkerNode);
+
+	opponentMarkerNode = initNode("opponentMarker", NO_IMAGE);
+	opponentMarkerNode.shape = initShapeBox(250.0F, 250.0F, 250.0F, DARK_RED, 1.0F);
+	push(&mapScene.nodes, &opponentMarkerNode);
 }
 
 static void startGame(void) {
+	isfinished = FALSE;
+	heroLapScore = 0;
+	heroPreviousLap = -1;
+	opponentLapScore = 0;
+	opponentPreviousLap = -1;
+
 	heroNode.position[0] = 650.0F;
 	heroNode.position[1] = 50.0F;
 	heroNode.position[2] = 0.0F;
 	clearVec3(heroNode.velocity);
 	clearVec3(heroNode.angle);
 	clearVec3(heroNode.angVelocity);
+	clearVec3(heroNode.angMomentum);
 
 	opponentNode.position[0] = 600.0F;
 	opponentNode.position[1] = 50.0F;
 	opponentNode.position[2] = 0.0F;
+	clearVec3(opponentNode.velocity);
+	clearVec3(opponentNode.angle);
+	clearVec3(opponentNode.angVelocity);
+	clearVec3(opponentNode.angMomentum);
 
 	clearVector(&scene.nodes);
 	pushUntilNull(&scene.nodes, &speedNode, &lapNode, &rankNode, &centerNode, &timeNode, NULL);
+	pushUntilNull(&scene.nodes, &mapNode, NULL);
 	pushUntilNull(&scene.nodes, &lapJudgmentNodes[0], &lapJudgmentNodes[1], &lapJudgmentNodes[2], NULL);
 	pushUntilNull(&scene.nodes, &heroNode, &opponentNode, NULL);
 	pushUntilNull(&scene.nodes, &courseNode, &courseDirtNode, NULL);
@@ -361,6 +434,7 @@ int loop(float elapsed, Image *out) {
 		if(!collisionFlag) {
 			int i;
 			for(i = 0;i < 3;i++) lapJudgmentNodes[i].isVisible = !lapJudgmentNodes[i].isVisible;
+			heroRayNode.isVisible = !heroRayNode.isVisible;
 			opponentRayNode.isVisible = !opponentRayNode.isVisible;
 			courseDirtNode.isVisible = !courseDirtNode.isVisible;
 			collisionFlag = TRUE;
@@ -370,10 +444,10 @@ int loop(float elapsed, Image *out) {
 	}
 	scene.camera.fov -= 0.05F * controller.arrow[1];
 	scene.camera.fov = min(PI / 3.0F * 2.0F, max(PI / 3.0F, scene.camera.fov));
-	addVec3(currentCameraPosition, mulVec3ByScalar(subVec3(nextCameraPosition, currentCameraPosition, tempVec3[0]), 2.0F * elapsed, tempVec3[1]), currentCameraPosition);
 	cameraAngle -= 0.1F * controller.arrow[0];
-	genRotationMat4(0.0F, cameraAngle, 0.0F, tempMat4[0]);
-	mulMat3Vec3(convMat4toMat3(tempMat4[0], tempMat3[0]), currentCameraPosition, scene.camera.position);
+	handle += 0.3F * (controller.move[0] - handle);
+	genRotationMat4(0.0F, cameraAngle + handle / 2.0F, 0.0F, tempMat4[0]);
+	mulMat3Vec3(convMat4toMat3(tempMat4[0], tempMat3[0]), defaultCameraPosition, scene.camera.position);
 	return TRUE;
 }
 
